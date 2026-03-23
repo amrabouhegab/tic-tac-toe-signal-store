d2aa80f4cedb018d9a9ad0725e7adc30c7896693
Amr Sadat <amrsadat2017@gmail.com>
2026-03-23 14:20:21 +0200

    Add winner modal

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { GameBoard } from './components/game-board/game-board';
+import { WinnerModal } from './components/winner-modal/winner-modal';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, GameBoard],
+  imports: [Header, Footer, GameBoard, WinnerModal],
   templateUrl: './app.html',
 })
 export class App {
---
+++ src/app/components/winner-modal/winner-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-winner-modal',
+  imports: [],
+  templateUrl: './winner-modal.html',
+  styleUrl: './winner-modal.css',
+})
+export class WinnerModal {}
